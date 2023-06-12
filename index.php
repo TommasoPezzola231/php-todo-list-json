de2dc00523241372f6718939ae6357455022da12
TommasoPezzola <pezzola123@gmail.com>
2023-06-12 14:50:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP to do list JSON</title>
+    <script src="https://unpkg.com/vue@3"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
+    <div id="app">
+        
+        <div class="container">
+            <h1>To Do List</h1>
+        </div>
+
+    </div>
+
+    <script src="./js/main.js"></script>
+
+</body>
+</html>
