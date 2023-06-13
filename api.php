dfe33b24e03c5ca642e71210b6847fe336d10e41
TommasoPezzola <pezzola123@gmail.com>
2023-06-13 17:33:49 +0200

    Error

--- api.php
+++ api.php
@@ -1,18 +1,18 @@
 <?php 
     $toDoList = file_get_contents("dati.json");
 
-    header("Content-Type: application/json");
-
+    
     $list = json_decode($toDoList, true);
     
     if ( isset($_POST["newTask"]) && $_POST["newTask"] != "") {
         $list[] = $_POST["newTask"];
         $toDoList = json_encode($list);
         file_put_contents("dati.json", $toDoList);
-
+        
     }
-
+    
     $toDoList = json_encode($list);
+    header("Content-Type: application/json");
 
     echo $toDoList;
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP to do list JSON</title>
     <script src="https://unpkg.com/vue@3"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
